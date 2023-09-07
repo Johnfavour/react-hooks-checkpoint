8859f75c8f661eaccd8436122ded000041b6549c
Johnfavour <favourjohn@gmail.com>
2023-09-07 09:56:52 +0100

    react-router-checkpoint

--- src/Components/MovieCard/MovieCard.jsx
+++ src/Components/MovieCard/MovieCard.jsx
@@ -1,15 +1,17 @@
 // MovieCard.js
 import React from 'react';
 import './MovieCard.css'
+import { Link } from 'react-router-dom';
 
 function MovieCard({ movie }) {
   return (
     <div className="moviecard">
-      <img src={movie.posterURL} alt={movie.title} style={{paddingTop: '15px'}}/>
-      <h2>{movie.title}</h2>
-      <p>{movie.description}</p>
-      <p style={{paddingBottom:'15px'}}>Rating: {movie.rating}</p>
-
+      <Link to={`/movie/${movie.title}`} style={{color:'#fff'}}>
+        <img src={movie.posterURL} alt={movie.title} style={{paddingTop: '15px'}}/>
+        <h2>{movie.title}</h2>
+        <p>{movie.description}</p>
+        <p style={{paddingBottom:'15px'}}>Rating: {movie.rating}</p>
+      </Link>
     </div>
   );
 }
